b520253e8893344c124b52d883e2a8b8bd758b6c
blkdmr <alessio.russo.ar98@gmail.com>
2025-12-02 22:35:49 +0100

    updated disclaimer

--- src/fenn/__init__.py
+++ src/fenn/__init__.py
@@ -40,7 +40,7 @@ class FENN:
             f"{Style.BRIGHT}Hi, thank you for using the {Fore.GREEN}PyFenn{Style.RESET_ALL}{Style.BRIGHT} framework.{Style.RESET_ALL}\n"
             f"PyFenn is still in an {Fore.YELLOW}alpha version{Style.RESET_ALL}.\n"
             "If you find a bug or inconsistency, if you want to contribute or request a feature,\nplease open an issue at "
-            f"{Fore.CYAN}https://github.com/blkdmr/fenn/issues{Style.RESET_ALL}\n"
+            f"{Fore.CYAN}https://github.com/pyfenn/fenn/issues{Style.RESET_ALL}\n"
             "Thank you for your support!\n"
             "***********************************************************************************\n"
         )
